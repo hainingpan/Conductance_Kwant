9a84056949e049cc04a5e6371f6a8c82f3056b17
Haining Pan <jack_pan1994@126.com>
2018-08-29 18:14:25 -0400

    20180829

--- test.py
+++ test.py
@@ -8,34 +8,34 @@ rank = comm.Get_rank();
 size=comm.Get_size();
 #size=1;
 #rank=0;
-tot=512;
+tot=8;
 
 NS_dict = {'a':1,'mu':.2,'alpha_R':5, 'Delta_0':0.2,'wireLength':100, 'mu_lead':25.0, 'Nbarrier':2,'Ebarrier':10.0, 'gamma':0.001, 'QD':'no', 'VD':0.8, 'dotLength':20, 'SE':'no', 'Vz':0.0, 'voltage':0.0,'varymu':'no', 'lamd':0};
 
 
 np.warnings.filterwarnings('ignore');
-voltageMin = -0.3; voltageMax = 0.3; voltageNumber = 1001;
+voltageMin = -0.3; voltageMax = 0.3; voltageNumber = 3;
 voltageRange = np.linspace(voltageMin, voltageMax, voltageNumber);
 
 per=int(tot/size);
-junction=Maj.NSjunction(NS_dict);
-VzStep = 0.002;    
+#VzStep = 0.002;  
+VzStep = 0.2;    
 sendbuf=np.empty((per,voltageNumber));
 for ii in range(per):    
     NS_dict['Vz'] = (ii+rank*per)*VzStep;
-#    gFile = open('G_mu'+str(NS_dict['mu'])+'_L'+str(NS_dict['wireLength'])+'_Vz'+ str(int(NS_dict['Vz']/VzStep))+'.txt','w');
-    
+    junction=Maj.NSjunction(NS_dict);
+#    gFile = open('G_mu'+str(NS_dict['mu'])+'_L'+str(NS_dict['wireLength'])+'_Vz'+ str(int(NS_dict['Vz']/VzStep))+'.txt','w');    
     for index in range(voltageNumber):
         voltage=voltageRange[index];
         NS_dict['voltage']=voltage;        
         sendbuf[ii,index]=Maj.conductance(NS_dict,junction);
-
 if (rank==0):
     recvbuf=np.empty((tot,voltageNumber));
 else:
     recvbuf=None;
 comm.Gather(sendbuf,recvbuf,root=0);
-np.savetxt('G_mu'+str(NS_dict['mu'])+'_L'+str(NS_dict['wireLength'])+'_Delta'+str(NS_dict['Delta_0'])+'_alpha_R'+str(NS_dict['alpha_R'])+'.txt',recvbuf);
+if (rank==0):
+    np.savetxt('G_mu'+str(NS_dict['mu'])+'_L'+str(NS_dict['wireLength'])+'_Delta'+str(NS_dict['Delta_0'])+'_alpha_R'+str(NS_dict['alpha_R'])+'.txt',recvbuf);
       
 #        gFile.write( str(Maj.conductance(NS_dict,junction)) + ',' );
 #    gFile.write('\n');
