0231b6ae7cd20cf20162a843228d9032d5f1df8b
Haining Pan <jack_pan1994@126.com>
2018-08-29 02:23:30 -0400

    20180829

--- Main.py
+++ Main.py
@@ -9,13 +9,14 @@ if (rank==0):
     a=np.zeros((4,4));
     a[rank,:]=np.ones(4)*(rank+1);
     data=np.empty(4);
-    for i in range(1,4)
-        comm.Recv(data,source=i,tag=0);
-        a[]
+    datar=[];
+    for i in range(1,4):
+        comm.Recv(datar,source=i,tag=0);
+        print(datar);
     print(a);
 else:
     data=np.ones(4)*(rank+1);
-    comm.Send(data,dest=0,tag=0);
+    comm.Send([data,rank],dest=0,tag=0);
 
     
     
