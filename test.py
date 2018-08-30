1ba11d209c9bc18ca9f4529994994df155c65c2b
Haining Pan <jack_pan1994@126.com>
2018-08-30 08:25:20 -0400

    20180830

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ import Majorana_module as Maj
 comm = MPI.COMM_WORLD;
 rank = comm.Get_rank();
 size=comm.Get_size();
-size=1;
-rank=0;
+#size=1;
+#rank=0;
 tot=512;
 
 NS_dict = {'a':1,'mu':0,'alpha_R':2, 'Delta_0':0.2,'Delta_c':0.2,'epsilon':1,'wireLength':3000, 'mu_lead':25.0, 'Nbarrier':2,'Ebarrier':10.0, 'gamma':0.001, 'QD':'no', 'VD':0.8, 'dotLength':20, 'SE':'no', 'Vz':0.0, 'voltage':0.0,'varymu':'no', 'lamd':0,'singleband':'no'};
