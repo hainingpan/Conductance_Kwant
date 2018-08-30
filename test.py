770fc241a2342c6afe1737d18df88f9a459f63ce
Haining Pan <jack_pan1994@126.com>
2018-08-30 00:25:18 -0400

    20180830

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
-#from mpi4py import MPI
+from mpi4py import MPI
 import numpy as np
 import Majorana_module as Maj
 
 
-#comm = MPI.COMM_WORLD;
-#rank = comm.Get_rank();
-#size=comm.Get_size();
+comm = MPI.COMM_WORLD;
+rank = comm.Get_rank();
+size=comm.Get_size();
 size=1;
 rank=0;
-tot=4;
+tot=512;
 
 NS_dict = {'a':1,'mu':0,'alpha_R':2, 'Delta_0':0.2,'Delta_c':0.2,'epsilon':1,'wireLength':100, 'mu_lead':25.0, 'Nbarrier':2,'Ebarrier':10.0, 'gamma':0.001, 'QD':'no', 'VD':0.8, 'dotLength':20, 'SE':'no', 'Vz':0.0, 'voltage':0.0,'varymu':'no', 'lamd':0,'singleband':'yes'};
 
 
 np.warnings.filterwarnings('ignore');
-voltageMin = -0.3; voltageMax = 0.3; voltageNumber = 3;
+voltageMin = -0.3; voltageMax = 0.3; voltageNumber = 1001;
 voltageRange = np.linspace(voltageMin, voltageMax, voltageNumber);
 
 per=int(tot/size);
@@ -31,7 +31,7 @@ if (rank==0):
     recvbuf=np.empty((tot,voltageNumber));
 else:
     recvbuf=None;
-#comm.Gather(sendbuf,recvbuf,root=0);
+comm.Gather(sendbuf,recvbuf,root=0);
 if (rank==0):
     if (NS_dict['singleband']=='yes'):
         fn='mu'+str(NS_dict['mu'])+'Delta'+str(NS_dict['Delta_0'])+'alpha'+str(NS_dict['alpha_R'])+'L'+str(NS_dict['wireLength'])+'.dat';     
