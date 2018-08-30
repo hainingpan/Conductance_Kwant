26518a70e79a1022f4eabdefe258e70d1fa041fc
Haining Pan <jack_pan1994@126.com>
2018-08-30 16:48:09 -0400

    .

--- Main.py
+++ Main.py
@@ -1,22 +1,28 @@
-from mpi4py import MPI
-import numpy as np
+#from mpi4py import MPI
+#import numpy as np
+#
+#comm = MPI.COMM_WORLD;
+#rank = comm.Get_rank();
+#size=comm.Get_size();
+#
+#
+# sendbuf=np.ones(4)*(rank+1)**2;
+#if (rank==0):
+#    recvbuf=np.empty((size,4));        
+#else:   
+#    recvbuf=None;
+#    
+#comm.Gather(sendbuf,recvbuf,root=0)
+#
+#if (rank==0):
+#    print(recvbuf)
+#
+#    
+#    
+#    
 
-comm = MPI.COMM_WORLD;
-rank = comm.Get_rank();
-size=comm.Get_size();
-
-
- sendbuf=np.ones(4)*(rank+1)**2;
-if (rank==0):
-    recvbuf=np.empty((size,4));        
-else:   
-    recvbuf=None;
+def main(a=1,b=2,c=3):
+    print('a=',a,'b=',b,'c=',c)
     
-comm.Gather(sendbuf,recvbuf,root=0)
-
-if (rank==0):
-    print(recvbuf)
-
-    
-    
-    
+if __name__=="__main__":
+	main()
