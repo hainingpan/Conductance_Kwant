e436c5f5f7fb27ce1ad8191c595ec51968a44059
Haining Pan <jack_pan1994@126.com>
2018-10-24 16:38:23 -0400

    fix singleband

--- Majorana_module.py
+++ Majorana_module.py
@@ -32,8 +32,8 @@ def NSjunction(args_dict):
             junction[lat(x)]=(-mu+2*t)*np.kron(np.array([[1,0],[0,0]]),PM.tzs0)+(epsilon-mu+2*t)*np.kron(np.array([[0,0],[0,1]]),PM.tzs0)+Delta_0*np.kron(PM.s0,PM.txs0)+Vz*np.kron(PM.s0,PM.t0sx)-1j*Gamma*np.kron(PM.s0,PM.t0s0)+Delta_c*np.kron(PM.sx,PM.txs0);
     #Construct hopping
     if args_dict['multiband']==0:
-        for x in range(wireLength):
-            junction[lat(x)]=(-mu+2*t)*PM.tzs0+Delta_0*PM.txs0+Vz*PM.t0sx-1j*Gamma*PM.t0s0;
+        for x in range(1,wireLength):
+            junction[lat(x-1),lat(x)]=-t*PM.tzs0-1j*alpha*PM.tzsy;
     else:
         for x in range(1,wireLength):
             junction[lat(x-1),lat(x)]=-t*np.kron(PM.s0,PM.tzs0)-1j*alpha*np.kron(PM.s0,PM.tzsy);
