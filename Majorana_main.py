7ad3376625eea9cd28a5b9c34b1ce3bfe30192f9
Haining Pan <jack_pan1994@126.com>
2018-10-25 18:01:11 -0400

    Update Majorana_main.py

--- Majorana_main.py
+++ Majorana_main.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 def main():
     vars=len(sys.argv);
-    NS_dict = {'a':1,'mu':.2,'mumax':.2,'alpha_R':2, 'Delta_0':0.2,'Delta_c':0.2,'epsilon':1,'wireLength':3000, 'mu_lead':25.0, 'Nbarrier':2,'Ebarrier':10.0, 'Gamma':0.0001, 'QD':'no', 'VD':0.4, 'dotLength':20, 'SE':'no', 'Vz':0.0, 'voltage':0.0,'smoothpot':0, 'gamma':0,'multiband':0,'leadpos':0};
+    NS_dict = {'a':1,'mu':.2,'mumax':1,'alpha_R':2, 'Delta_0':0.2,'Delta_c':0.2,'epsilon':1,'wireLength':3000, 'mu_lead':25.0, 'Nbarrier':2,'Ebarrier':10.0, 'Gamma':0.0001, 'QD':'no', 'VD':0.4, 'dotLength':20, 'SE':'no', 'Vz':0.0, 'voltage':0.0,'smoothpot':0, 'gamma':0,'multiband':0,'leadpos':0};
     if vars>1:        
         for i in range(1,vars):
             try:
@@ -32,16 +32,16 @@ def main():
     size=comm.Get_size();
 #    size=1;
 #    rank=0;
-    tot=4;  
+    tot=256;  
     if (rank==0):
         print(NS_dict);    
         
     np.warnings.filterwarnings('ignore');
-    voltageMin = -.3; voltageMax = .3; voltageNumber = 11;
+    voltageMin = -.3; voltageMax = .3; voltageNumber = 1001;
     voltageRange = np.linspace(voltageMin, voltageMax, voltageNumber);
     
     per=int(tot/size);
-    VzStep = 0.002*4;  
+    VzStep = 0.002*8;  
     sendbuf=np.empty((per,voltageNumber));
     for ii in range(per):
         NS_dict['Vz'] = (ii+rank*per)*VzStep;
