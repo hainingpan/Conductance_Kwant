80525f943c4fdf00b6c68592fe11839f552aa9b8
Haining Pan <jackpan1994@gmail.com>
2019-08-22 16:15:58 -0400

    fix bash script

--- yh.sh
+++ yh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mpirun -np 4 python Majorana_main.py Delta_0=0.9 wireLength=500 Ebarrier=1 Vc=6.7 mu=3 Gamma=0.01 vz0=2.5 vznum=256 vzstep=0.004 voltagemin=-0.4 voltagemax=0.4 leadnum=2
-mpirun -np 4 python Majorana_main.py Delta_0=0.9 wireLength=500 Ebarrier=1 Vc=6.7 mu=3 Gamma=0.01 vz0=2.5 vznum=256 vzstep=0.004 voltagemin=-0.4 voltagemax=0.4 leadnum=1 leadpos=1
+mpirun -np 8 python Majorana_main.py delta0=0.9 wireLength=500 barrierE=1 vc=6.7 mu=3 dissipation=0.01 vz0=2.5 vzNum=256 vzStep=0.004 vBiasMin=-0.4 vBiasMax=0.4 leadNum=2
+mpirun -np 8 python Majorana_main.py delta0=0.9 wireLength=500 barrierE=1 vc=6.7 mu=3 dissipation=0.01 vz0=2.5 vzNum=256 vzStep=0.004 vBiasMin=-0.4 vBiasMax=0.4 leadNum=1 leadPos=1
