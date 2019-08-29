72607288007991171dc9b00036b04655acc3f949
Haining Pan <jackpan1994@126.com>
2019-08-29 15:52:09 -0400

    clean

--- run2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mpirun -np 4 python Majorana_main.py vzNum=256  vzStep=0.008 leadNum=2 wireLength=100 qdPeak=1.1 isQD=1 dotLength=25 isSE=1 couplingSCSM=0.2 mu=1
-mpirun -np 4 python Majorana_main.py vzNum=256  vzStep=0.008 leadNum=1 leadpos=1 wireLength=100 qdPeak=1.1 isQD=1 dotLength=25 isSE=1 couplingSCSM=0.2 mu=1
-mpirun -np 4 python Majorana_main.py vzNum=256  vzStep=0.008 leadNum=2 wireLength=300 qdPeak=1.1 isQD=1 dotLength=20 isSE=1 couplingSCSM=0.2 mu=1
-mpirun -np 4 python Majorana_main.py vzNum=256  vzStep=0.008 leadNum=1 leadpos=1 wireLength=300 qdPeak=1.1 isQD=1 dotLength=20 isSE=1 couplingSCSM=0.2 mu=1
--- yh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mpirun -np 8 python Majorana_main.py delta0=0.9 wireLength=500 barrierE=1 vc=6.7 mu=3 dissipation=0.01 vz0=2.5 vzNum=256 vzStep=0.004 vBiasMin=-0.4 vBiasMax=0.4 leadNum=2
-mpirun -np 8 python Majorana_main.py delta0=0.9 wireLength=500 barrierE=1 vc=6.7 mu=3 dissipation=0.01 vz0=2.5 vzNum=256 vzStep=0.004 vBiasMin=-0.4 vBiasMax=0.4 leadNum=1 leadPos=1
